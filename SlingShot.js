cec976272af96c01585b855b43f52d945b8073f9
Kushaan Agarwal <64969376+kushaan20@users.noreply.github.com>
2020-06-01 16:56:24 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,29 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        //An array. The bird can fly anywhere. No fixed position.
+        this.sling.bodyA=null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+       
+    }
+    
+}
